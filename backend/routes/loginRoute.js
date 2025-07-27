9b5129beaaccb681dd64ec41ff4ee1c83541c67a
rohit-0807 <rohitjeerankalagi07@gmail.com>
2025-07-27 06:38:16 +0000

    loginRoute completed

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bycrypt from 'bcrypt';
-
+import jwt from 'jsonwebtoken';
 import user from '../modules/user.js';
+import dotenv from 'dotenv';
+
 const Router = express.Router();
 
 Router.post('/', async (req, res) => {
@@ -10,8 +12,31 @@ Router.post('/', async (req, res) => {
      if (!email || !password) {
        return res.status(400).json({error: "Please fill all the fields"});
      }
+     const existingUser = await user.findOne({email});
+     if(!existingUser) {
+        return res.status(400).json({error: "User not found"});
+     }
 
-   } catch(err) {
+     const isMatch = await bycrypt.compare(password, existingUser.password);
+     if(!isMatch) {
+        return res.status(400).json({message: "invalid credentials"});
+        } 
+
+        const token = jwt.sign(
+            {id: existingUser._id},
+            process.env.SECRET_KEY,
+            {expiresIn: '1h'}
+        );
+        
+        res.status(200).json({ token, 
+            user : {
+                id: existingUser._id,
+                name : existingUser.name,
+                email: existingUser.email,
+            },
+        });
+
+     } catch(err) {
     console.error(err.message);
     res.status(500).json({error : "somting wrong"});
    }

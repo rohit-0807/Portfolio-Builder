cea0eec702ead4c39c4277d68b88a75c8ebfe5ee
rohit-0807 <rohitjeerankalagi07@gmail.com>
2025-08-02 23:37:07 +0530

    login logic also and without css gives only message success or fail

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import SignupForm from './components/SignupForm'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   return (
     <div>
       <SignupForm />
+      <br />
+      <LoginForm />
     </div>
   )
 }

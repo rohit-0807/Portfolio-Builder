eb0fc7b6db5cdff3b3efc89464a10b5c85388952
rohit-0807 <rohitjeerankalagi07@gmail.com>
2025-09-15 17:20:15 +0530

    working on profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import SignupForm from './components/SignupForm'
-import LoginForm from './components/LoginForm'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import React from "react";
+import SignupForm from "./components/SignupForm";
+import LoginForm from "./components/LoginForm";
+import Profile from "./components/Profile";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 const App = () => {
   return (
     <div>
-      <SignupForm />
-      <br />
-      <LoginForm />
-    </div>
-  )
-}
+      <Router>
+        <nav>
+          <Link to="/signup">Signup</Link> |{" "} 
+          <Link to="/login">Login</Link> |{" "}
+          <Link to="/profile">Profile</Link>
+        </nav>
 
-export default App
+        <Routes>
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
